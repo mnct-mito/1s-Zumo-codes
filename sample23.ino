da2aebbaf17860935d2c5b8e20fda137791b8d37
mnct-mito <71414392+mnct-mito@users.noreply.github.com>
2023-12-02 14:17:38 +0900

    Create sample23.ino

---
+++ sample23.ino
@@ -0,0 +1,24 @@
+#include <Zumo32U4.h>
+
+#define MaxSpeed 400
+
+Zumo32U4ButtonA buttonA;
+Zumo32U4Motors motors;
+Zumo32U4LCD lcd;
+
+void setup()
+{
+    lcd.clear();
+    lcd.print("Press A");
+}
+
+void loop()
+{
+    // ˆÈ‰º‚ÌƒR[ƒh‚ğ“ü—Í‚µ‚È‚³‚¢
+    // «‚±‚±‚©‚ç
+
+
+
+
+    // ª‚±‚±‚Ü‚Å
+}
